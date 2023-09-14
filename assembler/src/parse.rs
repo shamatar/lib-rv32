588e9e5f5576d12610b05a34f791660867693757
Alexander <alex.m.vlasov@gmail.com>
2023-09-14 16:38:23 +0200

    cleanup sequence

--- assembler/src/parse.rs
+++ assembler/src/parse.rs
@@ -24,7 +24,7 @@ macro_rules! tokenize {
 pub fn match_opcode(op: &str) -> Result<u8, AssemblerError> {
     let opcode = match op {
         "add" | "sub" | "sll" | "srl" | "slt" | "sltu" | "xor" | "sra" | "or" | "and" => OPCODE_ARITHMETIC,
-        "addi" | "slli" | "srli" | "sltiu" | "slti" | "xori" | "srai" | "ori" | "andi" => OPCODE_ARITHMETIC_IMM,
+        "addi" | "slli" | "srli" | "slti" | "sltiu" | "xori" | "srai" | "ori" | "andi" => OPCODE_ARITHMETIC_IMM,
         "lui" => OPCODE_LUI,
         "auipc" => OPCODE_AUIPC,
         "jal" => OPCODE_JAL,
